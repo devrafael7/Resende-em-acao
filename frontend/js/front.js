dbce2e231529798eaad8a3d2332692bbb87c929f
Rafael <rafarafa99944@gmail.com>
2024-12-22 02:04:11 -0300

    desenvolvendo Pop up para index -> login

---
+++ frontend/js/front.js
@@ -0,0 +1,9 @@
+
+// Em desenvolvimento - Permanecer comentado para evitar erros no console
+
+//const popUpFazerLogin = document.querySelector('.popUpFazerLogin')
+//
+//const fecharPopUp = document.querySelector('.fecharPopUp')
+//fecharPopUp.addEventListener('click', ()=> {
+//    popUpFazerLogin.style.display = "none"
+//})
--- frontend/js/login.js
+++ frontend/js/login.js
@@ -45,4 +45,3 @@ loginForm.forEach( loginForms => {
     })
 })
 
-
