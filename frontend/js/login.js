f9f5835430e3f623c7b3241790f6697bdae4aed0
Rafael <rafarafa99944@gmail.com>
2024-12-21 22:18:11 -0300

    frontend e responsividade do login finalizado

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -9,3 +9,19 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+const signIn = document.querySelector('.sign-in')
+const signUp = document.querySelector('.sign-up')
+
+const dontHaveAccountDiv = document.querySelector('.dontHaveAccountDiv')
+const alreadyHaveAccountDiv = document.querySelector('.alreadyHaveAccountDiv')
+
+dontHaveAccountDiv.addEventListener('click', ()=> {
+    container.classList.add('active')
+})
+
+alreadyHaveAccountDiv.addEventListener('click', ()=> {
+    container.classList.remove('active')
+    signUp.classList.add('hiddenItem')
+})
+
