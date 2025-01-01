a81d41f8d822175785d90fac8993c41f5bdc0083
ItaloBRG84 <italobraga84@gmail.com>
2025-01-01 13:42:57 -0300

    criação do campo slides para instituições em html,css,js

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -17,3 +17,15 @@ if (user) {
     const userInfo = JSON.parse(decodeURIComponent(user));
     console.log('User Info:', userInfo);
 }
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+ function nextImage(){
+    count++;
+    if( count>5){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+ }
