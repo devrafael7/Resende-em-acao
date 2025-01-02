f631bb6740eee5e8ca292d58a65e3d5a86e302cb
ItaloBRG84 <italobraga84@gmail.com>
2025-01-02 18:07:49 -0300

    troca de aba de slides das instituições

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -18,14 +18,54 @@ if (user) {
     console.log('User Info:', userInfo);
 }
 
-let count = 1;
-document.getElementById("radio1").checked = true;
+// let count = 1;
+//document.getElementById("radio1").checked = true;
 
- function nextImage(){
-    count++;
-    if( count>5){
-        count = 1;
+// function nextImage(){
+//    count++;
+//    if( count>5){
+//        count = 1;
+//    }
+
+//    document.getElementById("radio"+count).checked = true;
+// }
+
+const slides = document.querySelectorAll('.slide');
+
+let currentSlide = 0;
+
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
+
+function updateSlide() {
+    // Esconde todos os slides
+    slides.forEach((slide, index) => {
+        slide.classList.remove('active');
+        if (index === currentSlide) {
+            slide.classList.add('active');
+        }
+    });
+
+    
+    prevButton.disabled = currentSlide === 0;
+    nextButton.disabled = currentSlide === slides.length - 1;
+}
+
+
+prevButton.addEventListener('click', () => {
+    if (currentSlide > 0) {
+        currentSlide--;
+        updateSlide();
     }
+});
 
-    document.getElementById("radio"+count).checked = true;
- }
+
+nextButton.addEventListener('click', () => {
+    if (currentSlide < slides.length - 1) {
+        currentSlide++;
+        updateSlide();
+    }
+});
+
+
+updateSlide();
